2340fff5361321e3695c0fa40c4a689257e0ce4a
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-06-29 09:16:01 +0530

    Added Screen Structure

--- lib/pages/bday_message.dart
+++ lib/pages/bday_message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BdayMessage extends StatefulWidget {
   const BdayMessage(this.restart, {super.key});
@@ -12,14 +13,29 @@ class BdayMessage extends StatefulWidget {
 }
 
 class _BdayMessage extends State<BdayMessage> {
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Text(
-            'Hello',
+          Text(
+            'Hey There,\nHere\'s A Message For You !',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.roboto(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Image.asset(
+            'assets/message.png',
+          ),
+          const SizedBox(
+            height: 60,
           ),
           OutlinedButton.icon(
             onPressed: widget.restart,
